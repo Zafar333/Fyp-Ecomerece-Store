18b313b955e545b7aaa060ca1759e45d35f89ac7
Arslan Zafar <arslanz499@gmail.com>
2023-07-24 23:52:45 +0500

    craete tailor shop done by arslan

---
+++ client/src/pages/Users/AllTailorsProfile/AllTailor/AllTailors.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../../../components/Navbar";
+
+const AllTailors = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="AllTailorsContainer"></div>
+    </>
+  );
+};
+
+export default AllTailors;
